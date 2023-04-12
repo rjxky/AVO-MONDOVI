9dedf806ec2142e5863052bd9e0f90b3b19a0f3e
rjxky <r.marcarino.0814@vallauri.edu>
2023-04-13 00:00:28 +0200

    commit

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,23 @@
 <?php
-  $name = $_POST['name'];
-  $surname = $_POST['surname'];
-  $date = $_POST['datanascita'];
-  $visitor_email = $_POST['email'];
-
-  $email_from = 'yourname@yourwebsite.com';
-
-	$email_subject = "New Form submission";
-
-	$email_body = "You have received a new message from the user $name.\n".
-                            "Here is the message:\n $message".
+ 
+$nome = $_POST['name'];
+$cognome = $_POST['surname'];
+$data_nascita = $_POST['datanascita'];
+$luogo_nascita = $_POST['luogonascita'];
+$numero_telefono = $_POST['phone'];
+ 
+$destinatario = 'ricky.marcarino@gmail.com';
+$oggetto = 'Nuova richiesta informazioni';
+$messaggio = "Nome: " . $nome . "\r\n" .
+            "Cognome: " . $cognome . "\r\n" .
+            "Data di nascita: " . $data_nascita . "\r\n" .
+            "Luogo di nascita: " . $luogo_nascita;
+            "Numero di telefono: " . $numero_telefono;
+ 
+$headers = 'From: ricky.marcarino@gmail.com' . "\r\n" .
+           'Reply-To: ricky.marcarino@gmail.com' . "\r\n" .
+           'X-Mailer: PHP/' . phpversion();
+ 
+           mail($destinatario, $oggetto, $messaggio, $headers)
+ 
 ?>
